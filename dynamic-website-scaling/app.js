3d836b31f34f8e10cda71bcea78e68e7c4ade396
Maximilian Schwarzmüller <28806196+maxschwarzmueller@users.noreply.github.com>
2022-08-09 07:51:11 +0200

    added dummy logic

--- dynamic-website-scaling/app.js
+++ dynamic-website-scaling/app.js
@@ -11,7 +11,17 @@ app.get('/', function (req, res) {
 
 app.post('/data', function (req, res) {
   const data = req.body;
-  res.status(201).json({ message: 'Received dummy data.', data });
+
+  let sum = 0;
+
+  for (let i = 0; i < 100000; i++) {
+    // dummy process to make the web app work harder
+    sum += i;
+  }
+
+  res
+    .status(201)
+    .json({ message: 'Received dummy data.', submitted: data, sum });
 });
 
 app.listen(3000);
